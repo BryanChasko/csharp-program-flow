fe84239db0971b6467674f19194b836207768d21
Bryan J Chasko <reddythafox@hotmail.com>
2021-09-04 23:04:13 -0600

    TaskReport.csv generated

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,5 +1,6 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -7,21 +8,71 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            GenerateWinnerEmails();
+
             GenerateTasksReport();
+
+            GenerateCommentsReport();
+        }
+
+        public static void GenerateWinnerEmails()
+        {
+            var selectedEmails = new List<string>();
+            int counter = 0;
+
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output:");
+            while (selectedEmails.Count < 2 && counter < Q1Results.Responses.Count)
+            {
+                var currentItem = Q1Results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == Q1Results.FavoriteProduct)
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
+        }
+
+        public static void GenerateCommentsReport()
+        {
+            var comments = new List<string>();
+
+            Console.WriteLine(Environment.NewLine + "Comments Output:");
+            for (var i = 0; i < Q1Results.Responses.Count; i++)
+            {
+                var currentResponse = Q1Results.Responses[i];
+
+                if (currentResponse.WouldRecommend < 7.0)
+                {
+                    Console.WriteLine(currentResponse.Comments);
+                    comments.Add(currentResponse.Comments);
+                }
+            }
+
+            foreach (var response in Q1Results.Responses)
+            {
+                if (response.AreaToImprove == Q1Results.AreaToImprove)
+                {
+                    Console.WriteLine(response.Comments);
+                    comments.Add(response.Comments);
+                }
+            }
+
+            File.WriteAllLines("CommentsReport.csv", comments);
         }
 
         public static void GenerateTasksReport()
         {
             var tasks = new List<string>();
-            //CALCULATED VALUES
-            // declaration statements to declare new variables followed by expression statements to assign values to the variables
+
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
-            //LOGICAL COMPARISONS
-            bool isCoffeeScoreLower = Q1Results.CoffeeScore < Q1Results.FoodScore;
-
-            if (isCoffeeScoreLower)
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
             {
                 tasks.Add("Investigate coffee recipes and ingredients.");
             }
@@ -34,39 +85,43 @@ namespace WiredBrainCoffeeSurveys.Reports
             {
                 tasks.Add("Work with employees for improvement ideas.");
             }
+
             if (responseRate < .33)
             {
                 tasks.Add("Research options to improve response rate.");
             }
             else if (responseRate > .33 && responseRate < .66)
             {
-                tasks.Add("Reward participants with free cofee coupon.");
+                tasks.Add("Reward participants with free coffee coupon.");
             }
-            //only possible left scenario would be if response rate is over .66
             else
             {
-                tasks.Add("Reward participants with discount cofee coupon.");
+                tasks.Add("Rewards participants with discount coffee coupon.");
             }
-            // SWITCH STATEMENT TO FIND USER FEEDBACK ON HOW TO IMPROVE
+
             switch (Q1Results.AreaToImprove)
             {
-                //break tells the program flow to exit out of the switch statement once matched case has been executed.
                 case "RewardsProgram":
                     tasks.Add("Revisit the rewards deals.");
                     break;
-
                 case "Cleanliness":
                     tasks.Add("Contact the cleaning vendor.");
                     break;
-
                 case "MobileApp":
-                    tasks.Add("Contact consulting firm about the app.");
+                    tasks.Add("Contact the consulting firm about app.");
                     break;
-
                 default:
                     tasks.Add("Investigate individual comments for ideas.");
                     break;
             }
+
+            Console.WriteLine(Environment.NewLine + "Tasks Output:");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
         }
     }
 }
